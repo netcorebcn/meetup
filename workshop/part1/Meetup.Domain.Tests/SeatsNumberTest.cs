a5646d71d2674e65d602710fa5a54ffa5ac113df
paulopez78 <pau.lopez.78@gmail.com>
2019-05-18 11:30:02 +0200

    super basic value object

--- workshop/part1/Meetup.Domain.Tests/SeatsNumberTest.cs
+++ workshop/part1/Meetup.Domain.Tests/SeatsNumberTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Meetup.Domain.Tests
@@ -17,5 +18,31 @@ namespace Meetup.Domain.Tests
         [InlineData(-1)]
         public void Given_Invalid_NumberOfSeats_When_Create_Then_Exception(int invalidSeatsNumber) =>
             Assert.Throws<ArgumentException>(() => SeatsNumber.From(invalidSeatsNumber));
+
+        [Fact]
+        public void Given_Same_NumberOfSeats_When_Compare_Then_Equal()
+        {
+            var number1 = SeatsNumber.From(10);
+            var number2 = SeatsNumber.From(10);
+
+            Assert.Equal(number1, number2);
+            Assert.True(number1 == number2);
+            Assert.False(number1 != number2);
+
+            var numbers = new Dictionary<SeatsNumber, string>();
+            numbers.Add(number1, "meetup1");
+            Assert.Throws<ArgumentException>(() => numbers.Add(number2, "meetup2"));
+        }
+
+        [Fact]
+        public void Given_Same_NumberOfSeats_When_Use_AsDictionaryKey_Then_Exception()
+        {
+            var number1 = SeatsNumber.From(10);
+            var number2 = SeatsNumber.From(10);
+            var numbers = new Dictionary<SeatsNumber, string>();
+
+            numbers.Add(number1, "meetup1");
+            Assert.Throws<ArgumentException>(() => numbers.Add(number2, "meetup2"));
+        }
     }
 }
--- workshop/part1/Meetup.Domain/SeatsNumber.cs
+++ workshop/part1/Meetup.Domain/SeatsNumber.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Meetup.Domain
 {
-    public class SeatsNumber
+    public class SeatsNumber : IEquatable<SeatsNumber>
     {
         public int Value { get; }
 
@@ -13,5 +13,24 @@ namespace Meetup.Domain
         }
 
         public static SeatsNumber From(int number) => new SeatsNumber(number);
+
+        public bool Equals(SeatsNumber other)
+        {
+            return this.Value == other.Value;
+        }
+
+        public override bool Equals(object other)
+        {
+            return this.Value == ((SeatsNumber)other).Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(SeatsNumber left, SeatsNumber right) => Equals(left, right);
+
+        public static bool operator !=(SeatsNumber left, SeatsNumber right) => !Equals(left, right);
     }
 }
